2dced95f23f2a3a3bdf340376070ec9fe0789e20
osvaldocastroo <osvaldocastro2704@gmail.com>
2024-08-14 20:21:15 -0300

    Agregamos validaciones

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,59 @@
+document.getElementById('regBtn').addEventListener('click', function(event) {
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+    var terminos = document.getElementById('terminos').checked;
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    
+    
+
+// Reset alerts
+hideAlerts();
+
+
+    // Validar contraseñas
+    if (password1 !== password2) {
+        showAlertError();
+        return;
+    }
+
+
+    // Validar términos y condiciones
+    if (!terminos) {
+        showAlertError();
+        return;
+    }
+
+    if(password1.length <= 5){ 
+        showAlertError(); 
+        return; 
+      } 
+
+      if(password2.length <= 5){ 
+        showAlertError(); 
+        return; 
+      } 
+
+      if(nombre === ""){
+        showAlertError();
+        return;
+      }
+
+    if(apellido === ""){
+        showAlertError();
+        return;
+    }
+
+    if(email === ""){
+        showAlertError();
+        return;
+    }
+
+    // Si todas las validaciones pasan
+    showAlertSuccess();
+});
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +61,10 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function hideAlerts() {
+    document.getElementById("alert-success").classList.remove("show");
+    document.getElementById("alert-danger").classList.remove("show");
+}
+
+
